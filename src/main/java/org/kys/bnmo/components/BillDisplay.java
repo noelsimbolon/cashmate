196fd78e3eaec3a075835cb86b0cb17ec8691e0f
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-04-26 06:57:43 +0700

    feat: add load helper

---
+++ src/main/java/org/kys/bnmo/components/BillDisplay.java
@@ -0,0 +1,10 @@
+package org.kys.bnmo.components;
+
+import javafx.scene.Parent;
+
+public class BillDisplay implements ComponentFactory {
+    @Override
+    public Parent getComponent() {
+        return null;
+    }
+}
