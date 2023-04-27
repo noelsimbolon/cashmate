4126edc2144ab2375744306238136ad754dbfae4
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2023-04-27 19:07:45 +0700

    chore: cleaning navbar code

--- src/main/java/org/kys/bnmo/components/NavBarFactory.java
+++ src/main/java/org/kys/bnmo/components/NavBarFactory.java
@@ -1,7 +1,5 @@
 package org.kys.bnmo.components;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
@@ -13,14 +11,13 @@ import org.kys.bnmo.helpers.StyleLoadHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class NavBarFactory implements ComponentFactory {
-    private VBox root;
-
     @Override
     public Parent getComponent() {
         // Main Vertical Box
-        root = new VBox(10);
+        VBox root = new VBox(10);
         root.setAlignment(Pos.TOP_CENTER);
 
         // Create The Components
@@ -33,28 +30,28 @@ public class NavBarFactory implements ComponentFactory {
         ArrayList<String> buttonNames = new ArrayList<>(Arrays.asList("Membership", "Cashier", "Catalogue", "Settings"));
         ArrayList<Button> buttonList = new ArrayList<>();
 
+        // Create Buttons Based On buttonNames and Add to the List
         for (String buttonName : buttonNames) {
             Button button = new Button(buttonName);
 
-            button.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    // Change Style to selected-nav-button
-                    // Also Change the Button Graphic
-                    for (int i = 0; i < buttonList.size(); i++) {
-                        if (buttonList.get(i) == event.getSource()) {
-                            buttonList.get(i).getStyleClass().clear();
-                            buttonList.get(i).getStyleClass().add("selected-nav-button");
-                            setButtonGraphic(buttonList.get(i), "/icon/" + buttonNames.get(i) + "SelectedImage.png");
-                        } else {
-                            buttonList.get(i).getStyleClass().clear();
-                            buttonList.get(i).getStyleClass().add("unselected-nav-button");
-                            setButtonGraphic(buttonList.get(i), "/icon/" + buttonNames.get(i) + "Image.png");
-                        }
+            // Set Button Action if Clicked
+            button.setOnAction(event -> {
+                // Change Style to selected-nav-button
+                // Also Change the Button Graphic
+                for (int i = 0; i < buttonList.size(); i++) {
+                    if (buttonList.get(i) == event.getSource()) {
+                        buttonList.get(i).getStyleClass().clear();
+                        buttonList.get(i).getStyleClass().add("selected-nav-button");
+                        setButtonGraphic(buttonList.get(i), "/icon/" + buttonNames.get(i) + "SelectedImage.png");
+                    } else {
+                        buttonList.get(i).getStyleClass().clear();
+                        buttonList.get(i).getStyleClass().add("unselected-nav-button");
+                        setButtonGraphic(buttonList.get(i), "/icon/" + buttonNames.get(i) + "Image.png");
                     }
                 }
             });
 
+            // Set Default Button Style and Graphic
             button.getStyleClass().add("unselected-nav-button");
             setButtonGraphic(button, "/icon/" + buttonName + "Image.png");
             buttonList.add(button);
@@ -73,7 +70,7 @@ public class NavBarFactory implements ComponentFactory {
 
     private void setButtonGraphic(Button button, String path) {
         // Set Button Graphic to Image that Located in the Path
-        Image image = new Image(getClass().getResource(path).toExternalForm());
+        Image image = new Image(Objects.requireNonNull(getClass().getResource(path)).toExternalForm());
         ImageView imageView = new ImageView(image);
         button.setGraphic(imageView);
     }
