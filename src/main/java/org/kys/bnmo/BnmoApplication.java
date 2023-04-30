7f9f72e9a1f9c8a744d80b8b054c6390665c0e31
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-05-01 02:40:36 +0700

    fix: maven build using shade plugin to build a fat jar

--- src/main/java/org/kys/bnmo/BnmoApplication.java
+++ src/main/java/org/kys/bnmo/BnmoApplication.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import org.kys.bnmo.components.bases.CheckoutPanel;
 import org.kys.bnmo.helpers.StyleLoadHelper;
 import org.kys.bnmo.views.Page;
 
@@ -57,6 +58,20 @@ public class BnmoApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+// Checkout panel testing
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        // Create a new instance of CheckoutPanel
+//        var checkoutPanel = new CheckoutPanel();
+//
+//        // Create a new scene with the root node of CheckoutPanel
+//        var scene = new Scene(checkoutPanel.getComponent(), 250, 326);
+//
+//        // Set the scene and show the stage
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/org/kys/bnmo/CashMate.java
@@ -0,0 +1,7 @@
+package org.kys.bnmo;
+
+public class CashMate {
+    public static void main(String[] args) {
+        BnmoApplication.main(args);
+    }
+}
