92e47e7800ea25c2bbeb6b877eee39725e06329d
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-05-01 05:05:00 +0700

    refactor: tabs for pages

--- src/main/java/org/kys/bnmo/BnmoApplication.java
+++ src/main/java/org/kys/bnmo/BnmoApplication.java
@@ -3,17 +3,27 @@ package org.kys.bnmo;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.kys.bnmo.components.bases.CheckoutPanel;
+import org.kys.bnmo.components.tabs.AddMemberTab;
+import org.kys.bnmo.components.tabs.BillTab;
+import org.kys.bnmo.components.tabs.ReportTab;
+import org.kys.bnmo.components.tabs.SettingTab;
 import org.kys.bnmo.helpers.StyleLoadHelper;
 import org.kys.bnmo.views.Page;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class BnmoApplication extends Application {
+
+    private Pane root;
 
     private void loadFonts(Parent root, String family, String ext)
     {
@@ -44,10 +54,29 @@ public class BnmoApplication extends Application {
             }
         }
     }
+
+    private List<Tab> getTabs()
+    {
+        return ((TabPane)root.lookup("parent-tab-pane")).getTabs();
+    }
+
+    private Optional<Tab> getTab(String id)
+    {
+        List<Tab> tabs = ((TabPane)root.lookup("parent-tab-pane")).getTabs();
+
+        return tabs.stream().filter(tab -> tab.getId() == id).findFirst();
+    }
+
     @Override
     public void start(Stage stage) {
         Page page = new Page();
-        Parent root = page.getComponent();
+        page.addTab(new AddMemberTab().getAndResetComponent(), "Membership");
+        page.addTab(new SettingTab().getAndResetComponent(), "Settings");
+        page.addTab(new BillTab().getAndResetComponent(), "Bill[id]");
+        page.addTab(new ReportTab().getAndResetComponent(), "Report[id]");
+
+        root = page.getAndResetComponent();
+
         StyleLoadHelper helper = new StyleLoadHelper("/styles/global.css");
         helper.load(root);
 
--- src/main/java/org/kys/bnmo/views/Page.java
+++ src/main/java/org/kys/bnmo/views/Page.java
@@ -4,21 +4,29 @@ import javafx.scene.Parent;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import org.kys.bnmo.components.*;
 import org.kys.bnmo.components.bases.Navbar;
 import org.kys.bnmo.components.tabs.AddMemberTab;
-import org.kys.bnmo.components.tabs.BillTab;
-import org.kys.bnmo.components.tabs.ReportTab;
-import org.kys.bnmo.components.tabs.SettingTab;
 import org.kys.bnmo.helpers.StyleLoadHelper;
 
-public class Page implements ComponentFactory {
+import java.util.List;
+
+public class Page extends ComponentBuilder {
     private Parent navBar;
     private TabPane tabPane;
     private Parent currentPage;
+
+    public void addTab(Parent content, String title)
+    {
+        Tab tab = new Tab(title);
+        tab.setContent(content);
+        tab.setId(title);
+
+        tabPane.getTabs().add(tab);
+    }
+
     @Override
-    public Pane getComponent() {
+    public void reset() {
         HBox root = new HBox();
 
         navBar = new Navbar().getComponent();
@@ -26,26 +34,14 @@ public class Page implements ComponentFactory {
 
         navBar.getStyleClass().add("navbar");
 
-        Tab tab = new Tab("Membership");
-        tab.setContent(new AddMemberTab().getAndResetComponent());
-
-        Tab tab2 = new Tab("Bill[id]");
-        tab2.setContent(new BillTab().getAndResetComponent());
-
-        Tab tab3 = new Tab("Report[id]");
-        tab3.setContent(new ReportTab().getAndResetComponent());
-
-        Tab tab4 = new Tab("Setting");
-        tab4.setContent(new SettingTab().getAndResetComponent());
-
-        tabPane.getTabs().addAll(tab4, tab3, tab2, tab);
-
         root.getChildren().addAll(navBar, tabPane);
         root.getStyleClass().add("page");
 
         StyleLoadHelper helper = new StyleLoadHelper("/styles/page.css");
         helper.load(root);
+        root.setId("parent-tab-pane");
 
-        return root;
+        setRoot(root);
     }
+
 }
