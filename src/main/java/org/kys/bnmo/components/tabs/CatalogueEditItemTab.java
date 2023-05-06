6fa7a16f9e62033c32da8889781a7865ebb45421
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-05-07 04:11:10 +0700

    feat: catalogue crud operations

--- src/main/java/org/kys/bnmo/components/tabs/CatalogueEditItemTab.java
+++ src/main/java/org/kys/bnmo/components/tabs/CatalogueEditItemTab.java
@@ -9,19 +9,17 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import org.jetbrains.annotations.NotNull;
 import org.kys.bnmo.components.bases.FormBuilder;
+import org.kys.bnmo.controllers.InventoryItemController;
 import org.kys.bnmo.helpers.views.IconButtonHelper;
 import org.kys.bnmo.model.InventoryItem;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class CatalogueEditItemTab extends TabContainer {
-
     private static final FormBuilder formBuilder = new FormBuilder();
-
     private static final IconButtonHelper iconButtonHelper = new IconButtonHelper();
-
-
-    EventHandler<ActionEvent> backButtonAction;
-    EventHandler<ActionEvent> saveButtonAction;
-    private InventoryItem editedItem;  // The item that is being edited
+    private static final InventoryItemController inventoryItemController = new InventoryItemController();
 
     // State saver
     private final StringProperty name;
@@ -29,10 +27,15 @@ public class CatalogueEditItemTab extends TabContainer {
     private final StringProperty stock;
     private final StringProperty price;
     private final StringProperty purchasePrice;
+    EventHandler<ActionEvent> backButtonAction;
+    EventHandler<ActionEvent> saveButtonAction;
+    private InventoryItem editedItem;  // The item that is being edited
 
-    public CatalogueEditItemTab(@NotNull InventoryItem editedItem, EventHandler<ActionEvent> backButtonAction) {
+    public CatalogueEditItemTab(@NotNull UUID editedItemID, EventHandler<ActionEvent> backButtonAction) {
+        ArrayList<InventoryItem> inventoryItems = inventoryItemController.readInventoryItems();
+
+        this.editedItem = inventoryItemController.getInventoryItemByUUID(inventoryItems, editedItemID);
         this.backButtonAction = backButtonAction;
-        this.editedItem = editedItem;
         this.name = new SimpleStringProperty(editedItem.getItemName());
         this.category = new SimpleStringProperty(editedItem.getCategory());
         this.stock = new SimpleStringProperty(editedItem.getStock().toString());
@@ -40,11 +43,19 @@ public class CatalogueEditItemTab extends TabContainer {
         this.purchasePrice = new SimpleStringProperty(editedItem.getPurchasePrice().toString());
 
         this.saveButtonAction = (event) -> {
-            System.out.println("Item Name: " + name.getValue());
-            System.out.println("Category: " + category.getValue());
-            System.out.println("Stock: " + stock.getValue());
-            System.out.println("Price: " + price.getValue());
-            System.out.println("Purchase Price: " + purchasePrice.getValue());
+            int editedItemIndex = inventoryItems.indexOf(editedItem);
+            inventoryItems.remove(editedItem);
+
+            editedItem.setItemName(name.getValue());
+            editedItem.setCategory(category.getValue());
+            editedItem.setStock(Integer.parseInt(stock.getValue()));
+            editedItem.setPrice(Integer.parseInt(price.getValue()));
+            editedItem.setPurchasePrice(Integer.parseInt(purchasePrice.getValue()));
+            editedItem.setImageFileName(category.getValue().toLowerCase() + ".png");
+
+            inventoryItems.add(editedItemIndex, editedItem);
+
+            inventoryItemController.writeInventoryItems(inventoryItems);
         };
     }
 
@@ -53,7 +64,7 @@ public class CatalogueEditItemTab extends TabContainer {
         // Initialize form to edit item
         formBuilder.addTitle("Item Details");
         formBuilder.addTextBox("Name", "Enter name", name);
-        formBuilder.addDropdown("Category", "Select category", new String[] {"Food", "Beverage", "Stationery", "Medicine"}, category);
+        formBuilder.addDropdown("Category", "Select category", new String[]{"Food", "Beverage", "Stationery", "Medicine"}, category);
         formBuilder.addTextBox("Stock", "Enter stock", stock);
         formBuilder.addTextBox("Price", "Enter price", price);
         formBuilder.addTextBox("Purchase Price", "Enter purchase price", purchasePrice);
