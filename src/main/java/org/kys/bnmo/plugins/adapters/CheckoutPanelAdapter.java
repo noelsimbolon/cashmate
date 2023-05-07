3e7f176a79dc210a7bb49ee150a623757f23ca18
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-05-08 01:21:49 +0700

    feat: checkout adapter

---
+++ src/main/java/org/kys/bnmo/plugins/adapters/CheckoutPanelAdapter.java
@@ -0,0 +1,11 @@
+package org.kys.bnmo.plugins.adapters;
+
+import org.kys.bnmo.components.bases.CheckoutPanel;
+
+public class CheckoutPanelAdapter {
+
+    private CheckoutPanel checkoutPanel;
+    public CheckoutPanelAdapter(CheckoutPanel checkoutPanel) {
+        this.checkoutPanel = checkoutPanel;
+    }
+}
