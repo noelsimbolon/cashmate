e761689e52bca40f2869797788a82d2afaf2e14f
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-05-07 12:51:45 +0700

    feat: settings can now modify datastore folder path and file format

--- src/main/java/org/kys/bnmo/components/tabs/SettingTab.java
+++ src/main/java/org/kys/bnmo/components/tabs/SettingTab.java
@@ -1,35 +1,67 @@
 package org.kys.bnmo.components.tabs;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.kys.bnmo.components.bases.FormBuilder;
+import org.kys.bnmo.controllers.SettingsController;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class SettingTab extends TabContainer {
     private static final FormBuilder formBuilder = new FormBuilder();
-
+    private static final SettingsController settingsController = new SettingsController();
     private Stage stage;
 
-    public SettingTab(Stage stage)
-    {
+    // State saver
+    private StringProperty folderPath;
+    private Property<String> fileFormat;
+    EventHandler<ActionEvent> saveButtonAction;
+
+    public SettingTab(Stage stage) {
+        settingsController.loadConfig();
+        this.folderPath = new SimpleStringProperty(settingsController.getFolderPath());
+        this.fileFormat = new SimpleStringProperty(settingsController.getFileFormat());
         this.stage = stage;
+
+        this.saveButtonAction = (event) -> {
+            System.out.println(folderPath.getValue());
+            System.out.println(fileFormat.getValue());
+
+            settingsController.loadConfig();
+
+            if (!Objects.equals(this.folderPath.getValue(), settingsController.getFolderPath())) {
+                try {
+                    settingsController.changeFolderPath(this.folderPath.getValue());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (!Objects.equals(this.fileFormat.getValue(), settingsController.getFileFormat())) {
+                settingsController.changeFileFormat(this.fileFormat.getValue());
+            }
+        };
     }
 
     @Override
     protected Pane getContent() {
 
         // Initialize form to add member
-        formBuilder.addFilePicker("Folder Location", stage);
-        formBuilder.addDropdown("Save file format", "Select the member level", new String[] {"JSON", "XML", "OBJ"});
-        formBuilder.addButton("Save", null);
-
-        // TODO: Connect save button to save the settings
+        formBuilder.addFolderPicker("Data Store Location", stage, folderPath);
+        formBuilder.addDropdown("Data Store Format", "Select a Format", new String[]{"json", "xml", "obj"}, fileFormat);
+        formBuilder.addButton("Save", saveButtonAction);
 
         return formBuilder.getAndResetComponent();
     }
 
     @Override
-    protected void additionalAction()
-    {
+    protected void additionalAction() {
         getRoot().getStyleClass().add("fill-tab-content");
         addHeaderTitle("Settings");
     }
